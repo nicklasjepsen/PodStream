5ae5034ad3d3c0d04141d7af2d011166aed118e3
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-05-23 21:42:05 +0200

    Add project files.

---
+++ PodStream/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PodStream.Startup))]
+namespace PodStream
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
