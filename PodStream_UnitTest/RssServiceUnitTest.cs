dc3a97936fdbdec1cfdaa298b912fd83ef69d708
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-05-24 00:52:18 +0200

    Added test for testing that rss elements are returned.

--- PodStream_UnitTest/RssServiceUnitTest.cs
+++ PodStream_UnitTest/RssServiceUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
 using PodStreamService;
 
@@ -7,6 +8,9 @@ namespace PodStream_UnitTest
     [TestFixture]
     public class RssServiceUnitTest
     {
+        private const string ValidFeedUrl = "http://www.dr.dk/mu/Feed/harddisken?format=podcast&limit=500";
+        private const string InvalidFeedUrl = "http://dr.dk/podcast404";
+
         [Test]
         public void TestGetRssItems_InvalidUrl()
         {
@@ -19,7 +23,18 @@ namespace PodStream_UnitTest
         public void TestGetRssItems_NotFound()
         {
             // Returns null if url is not found
-            Assert.AreEqual(null, new RssService().GetFeedItems("http://dr.dk/podcasts404"));
+            Assert.AreEqual(null, new RssService().GetFeedItems(InvalidFeedUrl));
+        }
+
+        [Test]
+        public void TestGetRssItems()
+        {
+            var rssItems = new RssService().GetFeedItems(ValidFeedUrl);
+
+            Assert.IsNotNull(rssItems);
+            Assert.IsNotNull(rssItems.GetRssChannels());
+            Assert.IsNotNull(rssItems.GetRssChannels().First());
+            Assert.IsTrue(rssItems.GetRssChannels().First().RssItems.Count > 0);
         }
     }
 }
