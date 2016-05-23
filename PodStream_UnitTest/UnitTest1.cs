a69aff474f94fe4e2f0a4437c0369226d1b0fb71
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-05-24 00:07:30 +0200

    Added test for null param.

---
+++ PodStream_UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+using PodStreamService;
+
+namespace PodStream_UnitTest
+{
+    [TestFixture]
+    public class PodcastFeedServiceUnitTest
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestGetFeedItems_ParamIsNull()
+        {
+            var service = new PodcastFeedService();
+            service.GetFeedItems(null);
+        }
+    }
+}
