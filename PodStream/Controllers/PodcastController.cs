8fa11b1d411d14735104a0d2cafcf4c51b7ce671
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-05-25 00:12:33 +0200

    Refactor removed unused code.

--- PodStream/Controllers/PodcastController.cs
+++ PodStream/Controllers/PodcastController.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using AutoMapper;
-using Newtonsoft.Json;
 using PodStream.Models;
 using PodStream.Providers;
-using PodStream.Providers.Models.PodcastApi;
 
 namespace PodStream.Controllers
 {
@@ -31,33 +28,18 @@ namespace PodStream.Controllers
         {
             var podcast = new Podcast
             {
-                AvailableChannels = GetChannels().Select(c => new SelectListItem { Text = c, Value = c }),
+                AvailableChannels = podcastProvider.GetChannels().Select(c => new SelectListItem { Text = c, Value = c }),
                 AvailableShows = new SelectListItem[0]
             };
 
             return View(podcast);
-
         }
 
-        private IEnumerable<string> GetChannels()
-        {
-            return podcastProvider.GetChannels();
-        }
-
-        //[HttpPost]
-        //[Route("Podcast/Episodes/{channel}/{show}")]
         public ActionResult GetEpisodes(string xmlUrl)
         {
-            //if (!shows.ContainsKey(show))
-            //    return HttpNotFound();
-            //return View(podcastFeedService.Get(shows[show]));
-            //return View();
             var res = podcastFeedService.Get(xmlUrl);
             return Json(res, JsonRequestBehavior.AllowGet);
         }
-
-        private Dictionary<string, string> shows = new Dictionary<string, string>();
-
 
         public ActionResult GetShows(string name)
         {
@@ -73,13 +55,6 @@ namespace PodStream.Controllers
                     Text = s.Title,
                     Value = s.XmlLink
                 }));
-
-            foreach (var show in allShows)
-            {
-                if (shows.ContainsKey(show.Title))
-                    continue;
-                shows.Add(show.Title, show.XmlLink);
-            }
 
             return Json(items, JsonRequestBehavior.AllowGet);
         }
